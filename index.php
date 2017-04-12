77d5f92cec40054aad8e4d4f8a5b2ecdadb52074
mllerose <r.martin.maze@gmail.com>
2017-04-12 15:56:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=microcms;charset=utf8','microcms_user','secret');
+$articles = $bdd->query('select *from t_article order by art_id desc');
+require 'view.php';
+require 'model.php'
+?>
